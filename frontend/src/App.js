9adc871c186fafb72017c48af085f77ec7e69039
Jess <jnryan7@hotmail.com>
2023-10-02 15:50:57 +1100

    completed the lab

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,29 @@
-import { useState } from 'react';
-import List from './components/List';
-import './App.css';
+import { useState, useEffect } from "react";
+import List from "./components/List";
+import "./App.css";
+
+// Summon the useEffect hook and load that tasks when the page is born anew -- instead of using a predefined list
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost/api/tasks")
+      .then((response) => response.json())
+      .then((data) => setTasks(data))
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }, []);
+  return (
+    <div className="page">
+      {" "}
+      <List heading="My Tasks" tasks={tasks} setTasks={setTasks} />{" "}
+    </div>
+  );
+}
+export default App;
+
+/*function App() {
 
     const [tasks, setTasks] = useState([
         { id: 1, description: 'Learn React', completed: true },
@@ -17,4 +38,4 @@ function App() {
     );
 }
 
-export default App;
+export default App; */
--- frontend/src/components/List.js
+++ frontend/src/components/List.js
@@ -1,10 +1,39 @@
 import { useState } from 'react';
 
 function Task(props) {
+  console.log(props);
 
-	console.log(props);
+  function onChange() {
+    const updatedTask = {
+      id: props.id,
+      description: props.description,
+      completed: !props.completed,
+    };
+    fetch(`http://localhost/api/tasks/${props.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updatedTask),
+    })
+      .then((response) => response.json())
+      .then(() => {
+        props.setTasks((tasks) =>
+          tasks.map((task) => {
+            if (task.id === props.id) {
+              return updatedTask;
+            } else {
+              return task;
+            }
+          })
+        );
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }
 
-	function onChange() {
+  /*function onChange() {
 		// Find the task we want to update and update it
 		props.setTasks(tasks => tasks.map(task => {
 			if (task.id === props.id) {
@@ -17,40 +46,92 @@ function Task(props) {
 				return task;
 			}
 		}));
-	}
+	} */
 
-	function onClick() {
-		// Find the task we want to delete and remove it
-		props.setTasks(tasks => tasks.filter(task => task.id !== props.id));
-	}
+  function onClick() {
+    // find the task we want to delete and remove it
+    fetch(`http://localhost/api/tasks/${props.id}`, {
+      method: "DELETE",
+    })
+	  .then(() => {
+		  // remove it from the state
+		  props.setTasks((tasks) => tasks.filter((task) => task.id !== props.id));
+	  })
+	  .catch((error) => {
+		  console.error("Error:", error);
+	});
+  
+  }
 
-	return (
-		<li><button type="button" onClick={onClick}>X</button> { props.description } <input type="checkbox" checked={props.completed} onChange={onChange}/></li>
-	);
+  return (
+  	<li><button type="button" onClick={onClick}>X</button>{props.description}<input type="checkbox" checked={props.completed} onChange={onChange} /></li>
+  );
 }
+/*
+  function onClick() {
+    // Find the task we want to delete and remove it
+    props.setTasks((tasks) => tasks.filter((task) => task.id !== props.id));
+  }
+
+  return (
+    <li>
+      <button type="button" onClick={onClick}>
+        X
+      </button>{" "}
+      {props.description}{" "}
+      <input type="checkbox" checked={props.completed} onChange={onChange} />
+    </li>
+  );
+}
+*/
 
 function List(props) {
+  const [newTask, setNewTask] = useState("");
 
-	const [newTask, setNewTask] = useState("");
+  function onChange(event) {
+    setNewTask(event.target.value);
+  }
 
-	function onChange(event) {
-		setNewTask(event.target.value);
-	}
+  // Creates a Method to store data in database (However: need to also create method to LOAD data from the database)
+  function onClick() {
+    fetch("http://localhost/api/tasks", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ description: newTask, completed: false }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        props.setTasks((tasks) => [...tasks, data]);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+    setNewTask("");
+  }
 
-	function onClick() {
+  /* function onClick() {
 		props.setTasks(tasks => [...tasks, { id: tasks.length + 1, description: newTask, completed: false }]);
-	}
+	} */
 
-	return (
-		<div>
-			<h1>{ props.heading }</h1>
-			<input type="text" placeholder="Add a new task" onChange={onChange} />
-			<button type="button" onClick={onClick}>Add</button>
-			<ul>
-				{ props.tasks.map(task => <Task setTasks={props.setTasks} id={task.id} description={task.description} completed={task.completed} />) }
-			</ul>
-		</div>
-	);
+  return (
+    <div>
+      <h1>{props.heading}</h1>
+      <input type="text" placeholder="Add a new task" onChange={onChange} />
+      <button type="button" onClick={onClick}>
+        Add
+      </button>
+      <ul>
+        {props.tasks.map((task) => (
+          <Task
+            setTasks={props.setTasks}
+            id={task.id}
+            description={task.description}
+            completed={task.completed}
+          />
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default List;
